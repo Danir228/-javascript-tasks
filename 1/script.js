95961ebc1928c193955e19dac28611d21fafbf59
Данир Низамов <bezymniy228@gmail.com>
2021-01-13 22:42:55 +0300

    Задача 3
    Добавление и удаление данных из input в table

--- 1/script.js
+++ 1/script.js
@@ -7,30 +7,44 @@
     };
 
     function getValue () {
-        let val = document.getElementById('input_text').value;
+        var val = document.getElementById('input_text').value;
         array = val.split(",");
     }
 
-    function squareSum(){
-        for (i = 0; i < array.length; i++) {
-            if (array[i] % 2 === 0) {
-                newArray.push(array[i]);
-            }
-        }
+    // function squareSum(){
+    //     for (i = 0; i < array.length; i++) {
+    //         if (array[i] % 2 === 0) {
+    //             newArray.push(array[i]);
+    //         }
+    //     }
 
-        document.write(newArray, `<br/>`);
+    //     document.write(newArray, `<br/>`);
 
-        var result = 0;
-        newArray.forEach(function(item) {
-            result += Math.sqrt(item);
-        });
-        return result;
-    }
+    //     var result = 0;
+    //     newArray.forEach(function(item) {
+    //         result += Math.sqrt(item);
+    //     });
+    //     return result;
+    // }
+
+    // document.write( squareSum() );
+
+    var tableText = document.getElementById("table_text");
 
     input_btn.onclick = function() {
         getValue ();
-        document.write( squareSum() );
+        for (i = 0; i < array.length; i++) {
+            var newTr = document.createElement ('tr'),
+                newTd = document.createElement ('td');
+            newTd.innerHTML = array[i];
+            newTr.append (newTd);
+            tableText.append (newTr);
+            // console.log (tableText.appendChild (newTr));
+        }
     };
+
+
+
 
 
 
