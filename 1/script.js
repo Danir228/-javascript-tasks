177675bfa45a80d343b8b4d89f5b132ebd8beb24
Данир Низамов <bezymniy228@gmail.com>
2021-01-15 19:01:10 +0300

    Задача 4
    Убрал баги, доработал и стилизовал табличку

--- 1/script.js
+++ 1/script.js
@@ -1,108 +1,94 @@
-    var array = [],
-    i;
+var array = [],
+i;
 
-    input_text.onkeyup = function () {
-        this.value = this.value.replace (/[^\d,.]/g, '');
-    };
+input_text.onkeyup = function () {
+    this.value = this.value.replace (/[^\d,.]/g, '');
+};
 
-    input_num.onkeyup = function () {
-        this.value = this.value.replace (/[^\d]/g, '');
-    };
+input_num.onkeyup = function () {
+    this.value = this.value.replace (/[^\d]/g, '');
+};
 
-    function getValue () {
-        var val = document.getElementById('input_text').value;
-        array = val.split(",");
+function getValue () {
+    var val = document.getElementById('input_text').value;
+    let newArr = val.split(",");
+    for (let i = 0; i < newArr.length; i++) {
+        array.push(newArr[i]);
     }
+}
 
-    function setValue () {
-        var value = document.getElementById('input_num').value;
-        array.push (value);
-        tableText.innerHTML = "";
-    }
+function setValue () {
+    var value = document.getElementById('input_num').value;
+    array.push (value);
+}
 
-    var tableText = document.getElementById('table_test').getElementsByTagName('tbody')[0];
+var tableText = document.getElementById('table_test').getElementsByTagName('tbody')[0];
 
-    function delElem(index) {
-        let arr = [];
-        for (i = 0; i < array.length; i++) {
-            if (i != index) {
-                arr.push (array[i]);
-            }
-        }
-        console.log(index);
-        array = arr;
-        tableText.innerHTML = "";
-        elemTbody();
-    }
-
-    function elemTbody() {
-        for (i = 0; i < array.length; i++) {
-            var newTr = document.createElement ('tr'),
-                newTd = document.createElement ('td');
-            newTd.innerHTML = array[i];
-            newTr.innerHTML = `<input type = 'button' onclick='delElem(${i})'  value = 'x'/>`;
-            newTr.append (newTd);
-            tableText.append (newTr);
+function delElem(index) {
+    let arr = [];
+    for (i = 0; i < array.length; i++) {
+        if (i != index) {
+            arr.push (array[i]);
         }
     }
+    console.log(index);
+    array = arr;
+    elemTbody();
+}
 
-    input_btn.onclick = function() {
-        getValue ();
-        elemTbody();
+function elemTbody() {
+    tableText.innerHTML = "";
+    for (i = 0; i < array.length; i++) {
+        var newTr = document.createElement ('tr');
+        newTr.innerHTML = `<td></td> <td>${array[i]}</td> <td><input type = 'button' onclick='delElem(${i})' value = 'x'/></td>`;
+        tableText.append (newTr);
+    }
+}
+
+input_btn.onclick = function() {
+    getValue ();
+    elemTbody();
+};
+
+input_sub.onclick = function() {
+    setValue ();
+    elemTbody();
+};
+
+let checkone = document.getElementById ("checkone"),
+    checktoo = document.getElementById ("checktoo"),
+    inputText = document.getElementById ("input_text"),
+    inputBtn = document.getElementById ("input_btn"),
+    inputNum = document.getElementById ("input_num"),
+    inputSub = document.getElementById ("input_sub"),
+    checkBoxes = document.getElementsByName('check');
+
+    var toogleInput = function(e){
+        target = e.target;
+        if (target.checked == true) {
+            inputText.disabled = false;
+            inputBtn.disabled = false;
+            inputNum.disabled = true;
+            inputSub.disabled = true;
+        }
     };
 
-    input_sub.onclick = function() {
-        setValue ();
-        elemTbody();
-    };
+toogleInput({target: checkone});
+checkone.addEventListener("change", toogleInput);
 
-    let checkone = document.getElementById ("checkone"),
-        checktoo = document.getElementById ("checktoo"),
-        inputText = document.getElementById ("input_text"),
-        inputBtn = document.getElementById ("input_btn"),
-        inputNum = document.getElementById ("input_num"),
-        inputSub = document.getElementById ("input_sub"),
-        checkBoxes = document.getElementsByName('check');
+var toogleInp = function(e){
+    target = e.target;
+    if (target.checked == true) {
+        inputNum.disabled = false;
+        inputSub.disabled = false;
+        inputText.disabled = true;
+        inputBtn.disabled = true;
 
-        var toogleInput = function(e){
-            target = e.target;
-            if (target.checked == true) {
-                inputText.disabled = false;
-                inputBtn.disabled = false;
-                inputNum.disabled = true;
-                inputSub.disabled = true;
-            }
-        };
+    } else {
+        inputNum.disabled = true;
+        inputSub.disabled = true;
+    }
+};
 
-        toogleInput({target: checkone});
-        checkone.addEventListener("change", toogleInput);
-
-
-        var toogleInp = function(e){
-            target = e.target;
-            if (target.checked == true) {
-                inputNum.disabled = false;
-                inputSub.disabled = false;
-                inputText.disabled = true;
-                inputBtn.disabled = true;
-
-            } else {
-                inputNum.disabled = true;
-                inputSub.disabled = true;
-            }
-        };
-
-        toogleInp({target: checktoo});
-        checktoo.addEventListener("change", toogleInp);
-
-
-
-
-
-
-
-
-
-
-
-
+toogleInp({target: checktoo});
+checktoo.addEventListener("change", toogleInp);
