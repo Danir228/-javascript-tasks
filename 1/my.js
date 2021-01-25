b582f2285b323096ac4b377008095b3d078bbf22
Данир Низамов <bezymniy228@gmail.com>
2021-01-25 23:54:27 +0300

    Задача9
    Решил задачки под номерами 10, 11, 12, 13, 14, 15, 16, 17. Условия задач в файле my.js

--- 1/my.js
+++ 1/my.js
@@ -1,18 +1,16 @@
 // Задача №1 - Даны 3 инпута и кнопка. По нажатию на кнопку получите числа, стоящие в этих инпутах и запишите их сумму в четвертый инпут
 
-
-
-var inputCreate = document.querySelector ('.item1_2');
+let inputCreate = document.querySelector ('.item1_2');
 
 function getValueInp() {
-    var result = 0;
-    var num1 = document.querySelector ('.item1_11').value;
+    let result = 0;
+    let num1 = document.querySelector ('.item1_11').value;
     result += parseInt(num1);
-    var num2 = document.querySelector ('.item1_12').value;
+    let num2 = document.querySelector ('.item1_12').value;
     result += parseInt(num2);
-    var num3 = document.querySelector ('.item1_13').value;
+    let num3 = document.querySelector ('.item1_13').value;
     result += parseInt(num3);
-    var inputResult = document.querySelector ('.item1_3');
+    let inputResult = document.querySelector ('.item1_3');
     inputResult.value = result;
 }
 
@@ -23,15 +21,15 @@ inputCreate.onclick = function() {
 // Задача №2 - Даны N инпутов с классом .num и кнопка.
 // По нажатию на кнопку получите числа, стоящие в этих инпутах и запишите их сумму в абзац с id="result"
 
-var inpCreate = document.querySelector ('.item2_2');
+let inpCreate = document.querySelector ('.item2_2');
 
 function getValueInp() {
-    var inpNumAll = document.getElementsByClassName ('num');
-    var result = 0;
-    for (i = 0; i < inpNumAll.length; i++) {
+    let inpNumAll = document.getElementsByClassName ('num');
+    let result = 0;
+    for (let i = 0; i < inpNumAll.length; i++) {
         result += parseInt(inpNumAll[i].value);
     }
-    var inputResult = document.getElementById ('result');
+    let inputResult = document.getElementById ('result');
     inputResult.innerHTML = result;
 }
 
@@ -41,16 +39,16 @@ inpCreate.onclick = function() {
 
 // Задача №3 - Дан инпут. В него вводится число. По потери фокуса найдите сумму цифр этого числа.
 
-var focusInp = document.getElementById ("focus");
+let focusInp = document.getElementById ("focus");
 
 function sumNewValue() {
-    var sum = 0;
-    var str = focusInp.value;
-	var newArray = str.split('');
-    for (var i = 0; i < newArray.length; i++) {
+    let sum = 0;
+    let str = focusInp.value;
+	let newArray = str.split('');
+    for (let i = 0; i < newArray.length; i++) {
 		sum += +newArray[i];
     }
-    var blurInp = document.getElementById ("focus_result");
+    let blurInp = document.getElementById ("focus_result");
     blurInp.innerHTML = sum;
 }
 
@@ -60,20 +58,20 @@ focusInp.onblur = function() {
 
 // Задача №4 - Дан инпут. В него вводятся числа через запятую. По потери фокуса найдите среднее арифметическое этих чисел (сумма делить на количество)
 
-loss_focus1.onkeyup = function () {
+let lossFocusFirst = document.getElementById ("loss_focus1");
+
+lossFocusFirst.onkeyup = function () {
     this.value = this.value.replace (/[^\d,.]/g, '');
 };
 
-var lossFocusFirst = document.getElementById ("loss_focus1");
-
 function focusFirst() {
-    var sum = 0;
-    var str = lossFocusFirst.value;
-	var newArray = str.split(',');
-    for (var i = 0; i < newArray.length; i++) {
+    let sum = 0;
+    let str = lossFocusFirst.value;
+	let newArray = str.split(',');
+    for (let i = 0; i < newArray.length; i++) {
 		sum += +newArray[i] / newArray.length;
     }
-    var resultFocusFirst = document.getElementById ("loss_focus1_result");
+    let resultFocusFirst = document.getElementById ("loss_focus1_result");
     resultFocusFirst.innerHTML = sum;
 }
 
@@ -83,14 +81,14 @@ lossFocusFirst.onblur = function() {
 
 // Задача №5 - Дан инпут. В него вводится ФИО через пробел. По потери фокуса запишите фамилию, имя и отчество в отдельные инпуты
 
-var lossFocusSecond = document.getElementById ("loss_focus2"),
+let lossFocusSecond = document.getElementById ("loss_focus2"),
     resultFocusLastName = document.getElementById ("loss_focus2_result1"),
     resultFocusName = document.getElementById ("loss_focus2_result2"),
     resultFocusMidleName = document.getElementById ("loss_focus2_result3");
 
 function focusName() {
-    var str = lossFocusSecond.value;
-	var newArr = str.split(' ');
+    let str = lossFocusSecond.value;
+	let newArr = str.split(' ');
     resultFocusLastName.value = newArr[0];
     resultFocusName.value = newArr[1];
     resultFocusMidleName.value = newArr[2];
@@ -102,16 +100,16 @@ lossFocusSecond.onblur = function() {
 
 //  Задача №6 - Дан инпут. В него вводится ФИО через пробел. ФИО вводится с маленькой буквы. Сделайте так, чтобы по потери фокуса инпутом, введенные фамилия, имя и отчество автоматически стали записанными с большой буквы (в том же инпуте).
 
-var lossFocusThird = document.getElementById ("loss_focus3");
+let lossFocusThird = document.getElementById ("loss_focus3");
 
 function fullName() {
-    var res = [];
-    var str = lossFocusThird.value;
-    var newArr = str.split(' ');
-    for (i = 0; i < newArr.length; i++) {
+    let res = [];
+    let str = lossFocusThird.value;
+    let newArr = str.split(' ');
+    for (let i = 0; i < newArr.length; i++) {
         res.push(newArr[i].toUpperCase());
     }
-    var lossFocThird = document.getElementById ("loss_focus3");
+    let lossFocThird = document.getElementById ("loss_focus3");
     lossFocThird.value = res.join(" ");
 }
 
@@ -121,15 +119,14 @@ lossFocusThird.onblur = function() {
 
 // Задача №7 - Дан инпут. В него вводится текст. По потери фокуса узнайте количество слов в этом тексте.
 
-var lossFocusFo = document.getElementById ("loss_focus4");
+let lossFocusFo = document.getElementById ("loss_focus4");
 
 function lenghtName() {
-    var len = 0;
-    var str = lossFocusFo.value;
-    var newArr = str.split(' ');
-    len += newArr.length;
-    var lossFocusRes = document.getElementById ("loss_focus4_result");
-    lossFocusRes.innerHTML = len;
+    let str = lossFocusFo.value;
+    let newArr = str.split(' ');
+    let lenghtWords = newArr.length;
+    let lossFocusRes = document.getElementById ("loss_focus4_result");
+    lossFocusRes.innerHTML = lenghtWords;
 }
 
 lossFocusFo.onblur = function() {
@@ -138,18 +135,18 @@ lossFocusFo.onblur = function() {
 
 // Задача №8 - Дан инпут. В него вводится текст. По потери фокуса узнайте количество символов в самом длинном слове в этом тексте.
 
-var lossFocusFive = document.getElementById ("loss_focus5");
+let lossFocusFive = document.getElementById ("loss_focus5");
 
 function maxLengthName() {
-    var len = '';
-    var str = lossFocusFive.value;
-    var newArr = str.split(' ');
-    for (i = 0; i < newArr.length; i++) {
+    let len = '';
+    let str = lossFocusFive.value;
+    let newArr = str.split(' ');
+    for (let i = 0; i < newArr.length; i++) {
         if (newArr[i].length > newArr.length) {
             len = newArr[i].length;
         }
     }
-    var lossFocusRes = document.getElementById ("loss_focus5_result");
+    let lossFocusRes = document.getElementById ("loss_focus5_result");
     lossFocusRes.innerHTML = len;
 }
 
@@ -159,12 +156,12 @@ lossFocusFive.onblur = function() {
 
 // Задача №9 - Дан инпут. В него вводится дата в формате 31.12.2016. По потери фокуса в этом же инпуте поставьте эту дату в формате 2016-12-31.
 
-var lossFocusSix = document.getElementById ("loss_focus6");
+let lossFocusSix = document.getElementById ("loss_focus6");
 
 function newDateFormat() {
-    var str = lossFocusSix.value;
-    var newArr = str.split('.');
-    var sorted = newArr.reverse();
+    let str = lossFocusSix.value;
+    let newArr = str.split('.');
+    let sorted = newArr.reverse();
     lossFocusSix.value = sorted.join('-');
 }
 
@@ -172,3 +169,145 @@ lossFocusSix.onblur = function() {
     newDateFormat();
 };
 
+// Задача №10 - Дан инпут. В него вводится год рождения пользователя. По нажатию на кнопку выведите в абзац ниже сколько пользователю лет.
+
+let lossFocusSeven = document.getElementById ("loss_focus7");
+
+function howManyYeas() {
+    let d = new Date();
+    let year = d.getFullYear();
+    let val = lossFocusSeven.value;
+    let result = year - parseInt(val);
+    let paragraph = document.getElementById ("loss_focus7_result");
+    paragraph.textContent = result;
+}
+
+lossFocusSeven.onblur = function() {
+    howManyYeas();
+};
+
+// Задача №11 - Дан инпут. В него вводится дата в формате 31.12.2016. По потери фокуса узнайте день недели (словом), который приходится на эту дату.
+
+function dayWeek() {
+    let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+    let result = new Date (document.getElementById ("loss_focus8").value);
+    let res = days[result.getDay()];
+    let paragraph = document.getElementById ("loss_focus8_result");
+    paragraph.textContent = res;
+}
+
+
+
+let lossFocusEight = document.getElementById ("loss_focus8");
+
+lossFocusEight.onblur = function() {
+    dayWeek();
+};
+
+
+// Задача №12 - Дан инпут. В него вводится слово. По нажатию на кнопку проверьте то, что это слово читается с начала и с конца одинаково (например, мадам).
+
+let lossFocusNine = document.getElementById ("loss_focus9");
+
+function similarWord() {
+    let count = 0;
+    let val = lossFocusNine.value;
+    let arr = val.split("");
+    for (let i = 0; i < arr.length; i++) {
+        let k = arr.length - 1 - i;
+        if (arr[i] == arr[k]) {
+            count += 1;
+        }
+    }
+    if (arr.length == count) {
+        document.getElementById ("loss_focus9_result").textContent = 'Одинаково';
+    } else {
+        document.getElementById ("loss_focus9_result").textContent = 'Неодинаково';
+    }
+}
+
+let lossFocusNineSubmit = document.getElementById ("loss_focus10");
+
+lossFocusNineSubmit.onclick = function() {
+    similarWord();
+};
+
+// Задача №13 - Дан инпут. В него вводится число. Проверьте по вводу, что это число содержит внутри себя цифру 3.
+
+let lossFocusEleven = document.getElementById ("loss_focus11");
+
+lossFocusEleven.onkeyup = function () {
+    this.value = this.value.replace (/[^\d,.]/g, '');
+};
+
+function checkNum() {
+    let arr = lossFocusEleven.value;
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] == 3) {
+            document.getElementById ("loss_focus11_result").textContent = 'Цифра 3 присутствует';
+            break;
+        } else {
+            document.getElementById ("loss_focus11_result").textContent = 'Цифра 3 отсутствует';
+        }
+    }
+}
+
+lossFocusEleven.oninput = function() {
+    checkNum();
+};
+
+// Задача №14 - Даны N абзацев и кнопка. По нажатию на кнопку запишите в конец каждого абзаца его порядковый номер.
+
+const allParagraphs = document.querySelectorAll ('.backg'),
+      btnNum = document.getElementById ("loss_focus12");
+
+btnNum.addEventListener('click', () => {
+    let num = 1;
+    allParagraphs.forEach(function(item) {
+        item.insertAdjacentHTML('beforeend', `<span>№:${num}</span>`);
+        num++;
+    });
+});
+
+// Задача №15 - Даны N абзацев с числами. По нажатию на кнопку выведите эти числа в инпут через запятую в порядке возрастания.
+
+const nums = document.querySelectorAll('.nums'),
+      buttonNum = document.getElementById("loss_focus13"),
+      unpResultNums = document.getElementById("loss_focus14");
+
+function sortedNum(a, b) {
+    if(a < b) return -1;
+}
+
+buttonNum.addEventListener('click', () => {
+    let array = [];
+    [].forEach.call(nums, el => {
+        let number = el.textContent;
+        let res = parseInt(number);
+        array.push(res);
+    });
+    let result = array.sort(sortedNum);
+    unpResultNums.value = result.join(",");
+});
+
+// Задача №16 - Даны ссылки. По загрузке страницы добавьте в конец каждой ссылки ее href в круглых скобках.
+
+const linksItem = document.querySelectorAll(".links1_item");
+
+window.addEventListener('load', () => {
+    linksItem.forEach(function(item) {
+        item.insertAdjacentHTML('beforeend', ` <span>(${item.href})</span>`);
+    });
+});
+
+// Задача №17 - Даны ссылки. По загрузке страницы, если ссылка начинается с http://, то добавьте ей в конец стрелку → (делается так: &rarr;).
+
+const links = document.querySelectorAll(".links2_item");
+
+window.addEventListener('load', () => {
+    links.forEach(function(item) {
+        if(item.href.indexOf('https://') != -1) {
+            item.insertAdjacentHTML('beforeend', ` <span>&rarr;</span>`);
+        }
+    });
+});
